2f7a7fe049e9c1d54edf56b3f314b9e3a016de2e
Barb Banbury <darwinthesun@gmail.com>
2015-05-22 10:12:04 -0700

    adding NAMESPACE and bb_gecco.R

---
+++ R/bb_gecco.R
@@ -0,0 +1,19 @@
+#' bb_gecco
+#' 
+#' bb_gecco: Barbs functions for fetching gecco and isacc data
+#' 
+#' \tabular{ll}{
+#' Package: \tab bb_gecco \cr
+#' Type: \tab Package \cr
+#' Version: \tab 1.0 \cr
+#' Date: \tab 2015-05-22 \cr
+#' License: \tab GPL(>= 2) \cr
+#' }
+#' @aliases gecco
+#' @author Barb Banbury
+#' @docType package
+#' @name bb_gecco
+#' @import ncdf
+#' @import ncdf4
+#' @import Rmpi
+NULL
