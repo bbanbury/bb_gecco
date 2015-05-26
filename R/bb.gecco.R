72d5d7c97ea287df1459f7ac19b285a63e10e5c4
Barb Banbury <darwinthesun@gmail.com>
2015-05-26 08:07:22 -0700

    updating name to Rgecco

--- R/bb.gecco.R
+++ R/bb.gecco.R
@@ -1,18 +1,18 @@
-#' bb_gecco
+#' Rgecco
 #' 
-#' bb_gecco: Barbs functions for fetching gecco and isacc data
+#' Rgecco: Barbs functions for fetching gecco and isacc data
 #' 
 #' \tabular{ll}{
-#' Package: \tab bb_gecco \cr
+#' Package: \tab Rgecco \cr
 #' Type: \tab Package \cr
 #' Version: \tab 1.0 \cr
 #' Date: \tab 2015-05-22 \cr
 #' License: \tab GPL(>= 2) \cr
 #' }
-#' @aliases gecco
+#' @aliases rgecco
 #' @author Barb Banbury
 #' @docType package
-#' @name bb_gecco
+#' @name Rgecco
 #' @import ncdf
 #' @import ncdf4
 #' @import Rmpi
